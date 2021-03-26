afae190ec8a72105bf728c52169abca131eb133c
raychong <raychongtk@gmail.com>
2021-03-27 01:01:02 +0800

    add solutions

---
+++ src/BinaryTreeInorderTraversal.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author raychong
+ */
+public class BinaryTreeInorderTraversal {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        inorder(root, result);
+        return result;
+    }
+
+    private void inorder(TreeNode node, List<Integer> result) {
+        if (node == null) return;
+
+        inorder(node.left, result);
+        result.add(node.val);
+        inorder(node.right, result);
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
---
+++ src/CommonElementsInArray.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * @author raychong
+ */
+public class CommonElementsInArray {
+    public static void main(String[] args) {
+        System.out.println(commonElements(new int[]{4, 7, 3, 9, 2}, new int[]{3, 2, 12, 9, 40, 32, 4}));
+    }
+
+    public static List<Integer> commonElements(int[] num1, int[] num2) {
+        List<Integer> elements = new ArrayList<>();
+        Set<Integer> set = new HashSet<>();
+        for (int num : num1) {
+            set.add(num);
+        }
+
+        for (int num : num2) {
+            if (set.contains(num)) {
+                elements.add(num);
+            }
+        }
+
+        return elements;
+    }
+}
