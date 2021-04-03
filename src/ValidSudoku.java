1e1c0970b2332249757fc0a17847238d0897b590
raychong <raychongtk@gmail.com>
2021-04-03 22:07:06 +0800

    valid sudoku

---
+++ src/ValidSudoku.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * @author raychong
+ */
+public class ValidSudoku {
+    // https://leetcode.com/problems/valid-sudoku/
+    public boolean isValidSudoku(char[][] board) {
+        List<Set<Character>> rows = new ArrayList<>();
+        List<Set<Character>> columns = new ArrayList<>();
+        List<Set<Character>> partialSudoku = new ArrayList<>();
+        for (int i = 0; i < 9; i++) {
+            rows.add(new HashSet<>());
+            columns.add(new HashSet<>());
+            partialSudoku.add(new HashSet<>());
+        }
+
+        int rowBucket = 0;
+        for (int i = 0; i < board.length; i++) {
+            int columnBucket = 0;
+            for (int j = 0; j < board[i].length; j++) {
+                if (i != 0 && j == 0 && i % 3 == 0) rowBucket += 3;
+                if (j != 0 && j % 3 == 0) columnBucket++;
+                if (board[i][j] == '.') continue;
+                if (!rows.get(i).add(board[i][j]) || !columns.get(j).add(board[i][j])) return false;
+                if (!partialSudoku.get(rowBucket + columnBucket).add(board[i][j])) return false;
+            }
+        }
+
+        return true;
+    }
+}
