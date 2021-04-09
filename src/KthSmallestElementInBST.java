371d3fe202ab94377fa070cc4ed2cdcac5ea046d
raychong <raychongtk@gmail.com>
2021-04-09 22:16:20 +0800

    kth smallest element in a bst

---
+++ src/KthSmallestElementInBST.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author raychong
+ */
+public class KthSmallestElementInBST {
+    public int kthSmallest(TreeNode root, int k) {
+        List<Integer> result = new ArrayList<>();
+
+        inorder(root, result);
+
+        return result.get(k - 1);
+    }
+
+    private void inorder(TreeNode node, List<Integer> result) {
+        if (node == null) return;
+
+        inorder(node.left, result);
+        result.add(node.val);
+        inorder(node.right, result);
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
