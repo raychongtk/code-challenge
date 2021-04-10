1c8a391e94e2b03efd41d7aa078dcd166af60f80
raychong <raychongtk@gmail.com>
2021-04-10 12:10:56 +0800

    use one stack

--- src/DailyTemperatures.java
+++ src/DailyTemperatures.java
@@ -10,17 +10,13 @@ public class DailyTemperatures {
     // your output should be [1, 1, 4, 2, 1, 1, 0, 0].
     public int[] dailyTemperatures(int[] T) {
         int[] result = new int[T.length];
-        Deque<Integer> temperatures = new ArrayDeque<>();
         Deque<Integer> temperaturePositions = new ArrayDeque<>();
 
         for (int i = 0; i < T.length; i++) {
-            while (!temperatures.isEmpty() && temperatures.peek() < T[i]) {
-                temperatures.pop();
-                int index = temperaturePositions.pop();
-
-                result[index] = i - index;
+            while (!temperaturePositions.isEmpty() && T[temperaturePositions.peek()] < T[i]) {
+                int position = temperaturePositions.pop();
+                result[position] = i - position;
             }
-            temperatures.push(T[i]);
             temperaturePositions.push(i);
         }
 
