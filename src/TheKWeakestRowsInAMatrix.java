73061d7b739eab4073730e12a108147c1e287bd3
raychong <raychongtk@gmail.com>
2021-04-11 22:52:43 +0800

    use priority queue

--- src/TheKWeakestRowsInAMatrix.java
+++ src/TheKWeakestRowsInAMatrix.java
@@ -1,28 +1,20 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.PriorityQueue;
 
 /**
  * @author raychong
  */
 public class TheKWeakestRowsInAMatrix {
     public int[] kWeakestRows(int[][] mat, int k) {
-        int[] result = new int[k];
-        Map<Integer, List<Integer>> table = new TreeMap<>();
+        PriorityQueue<Pair> priorityQueue = new PriorityQueue<>();
         for (int i = 0; i < mat.length; i++) {
             int index = findRightMostIndex(mat[i]);
-            List<Integer> values = table.getOrDefault(index, new ArrayList<>());
-            values.add(i);
-            table.put(index, values);
+            priorityQueue.offer(new Pair(i, index));
         }
 
-        int i = 0;
-        for (Map.Entry<Integer, List<Integer>> entry : table.entrySet()) {
-            for (Integer data : entry.getValue()) {
-                result[i++] = data;
-                if (i == k) return result;
-            }
+        int[] result = new int[k];
+        for (int i = 0; i < k; i++) {
+            if (priorityQueue.isEmpty()) break; // prevent NPE
+            result[i] = priorityQueue.poll().rowIndex;
         }
 
         return result;
@@ -45,5 +37,21 @@ public class TheKWeakestRowsInAMatrix {
         }
 
         return index;
+    }
+
+    public static class Pair implements Comparable<Pair> {
+        int rowIndex;
+        int solider;
+
+        public Pair(int rowIndex, int solider) {
+            this.rowIndex = rowIndex;
+            this.solider = solider;
+        }
+
+        @Override
+        public int compareTo(Pair pair) {
+            int compare = Integer.compare(this.solider, pair.solider);
+            return compare == 0 ? Integer.compare(this.rowIndex, pair.rowIndex) : compare;
+        }
     }
 }
