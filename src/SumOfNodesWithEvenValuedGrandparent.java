0cedd68fecc96910d134971066379359235602ee
raychong <raychongtk@gmail.com>
2021-04-24 21:48:26 +0800

    sum of nodes with even valued grandparent

---
+++ src/SumOfNodesWithEvenValuedGrandparent.java
@@ -0,0 +1,41 @@
+/**
+ * @author raychong
+ */
+public class SumOfNodesWithEvenValuedGrandparent {
+    public int sumEvenGrandparent(TreeNode root) {
+        return traverse(root, -1, -1);
+    }
+
+    private int traverse(TreeNode node, int parent, int current) {
+        if (node == null) return 0;
+
+        int grandparent = parent;
+        parent = current;
+
+        int sum = 0;
+        if (grandparent % 2 == 0) sum = node.val;
+
+        sum += traverse(node.left, parent, node.val);
+        sum += traverse(node.right, parent, node.val);
+        return sum;
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
