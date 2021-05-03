455f2b9ba8712affd17603105e892bc904714148
raychong <raychongtk@gmail.com>
2021-05-03 20:21:44 +0800

    running sum

---
+++ src/array/RunningSum.java
@@ -0,0 +1,13 @@
+package array;
+
+/**
+ * @author raychong
+ */
+public class RunningSum {
+    public int[] runningSum(int[] nums) {
+        for (int i = 1; i < nums.length; i++) {
+            nums[i] += nums[i - 1];
+        }
+        return nums;
+    }
+}
