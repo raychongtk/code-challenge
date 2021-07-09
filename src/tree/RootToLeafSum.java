678430fbb835206d1ee80382e6f6c73dab182874
raychong <raychongtk@gmail.com>
2021-07-10 02:51:26 +0800

    root to leaf sum

---
+++ src/tree/RootToLeafSum.java
@@ -0,0 +1,34 @@
+package tree;
+
+/**
+ * @author raychong
+ */
+public class RootToLeafSum {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if (root == null) return false;
+        if (targetSum - root.val == 0 && root.left == null && root.right == null) {
+            return true;
+        }
+        return hasPathSum(root.left, targetSum - root.val)
+                || hasPathSum(root.right, targetSum - root.val);
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
