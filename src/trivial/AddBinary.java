f762276db3f27e5393c3fb1f12c952c273cd49e9
raychong <raychongtk@gmail.com>
2021-08-03 23:51:47 +0800

    add binary strings

--- src/trivial/AddBinary.java
+++ src/trivial/AddBinary.java
@@ -8,9 +8,26 @@ import java.math.BigInteger;
 public class AddBinary {
     public static void main(String[] args) {
         System.out.println(addBinary("11", "1"));
+        System.out.println(addBinaryV2("11", "1"));
     }
 
     public static String addBinary(String a, String b) {
         return new BigInteger(a, 2).add(new BigInteger(b, 2)).toString(2);
+    }
+
+    public static String addBinaryV2(String s1, String s2) {
+        int i = s1.length() - 1;
+        int j = s2.length() - 1;
+        int carry = 0;
+        var result = new StringBuilder();
+
+        while (i >= 0 || j >= 0 || carry == 1) {
+            if (i >= 0) carry += s1.charAt(i--) - '0';
+            if (j >= 0) carry += s2.charAt(j--) - '0';
+            result.insert(0, carry % 2);
+            carry /= 2;
+        }
+
+        return result.toString();
     }
 }
