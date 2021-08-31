19cad990e1803f93e3d6af5ef4d4b249ab91d10b
raychong <raychongtk@gmail.com>
2021-08-31 23:13:11 +0800

    move zeros

--- src/array/MoveZeros.java
+++ src/array/MoveZeros.java
@@ -12,12 +12,10 @@ public class MoveZeros {
         int index = 0;
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] != 0) {
+                int temp = nums[index];
                 nums[index++] = nums[i];
+                nums[i] = temp;
             }
-        }
-
-        for (int j = index; j < nums.length; j++) {
-            nums[j] = 0;
         }
     }
 }
