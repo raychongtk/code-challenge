24fa7f3487c1657349d3c73b2ac1fd9d6b09d1a0
raychong <raychongtk@gmail.com>
2021-09-04 21:48:57 +0800

    add two number

--- src/linkedlist/AddTwoNumber.java
+++ src/linkedlist/AddTwoNumber.java
@@ -1,49 +1,51 @@
 package linkedlist;
 
-import java.math.BigInteger;
-
 /**
  * @author raychong
  */
 public class AddTwoNumber {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        BigInteger firstNumber = reverseNumber(getNumber(l1));
-        BigInteger secondNumber = reverseNumber(getNumber(l2));
-        BigInteger answer = firstNumber.add(secondNumber);
-        return buildNode(answer);
-    }
-
-    public String getNumber(ListNode node) {
-        BigInteger number = BigInteger.ONE;
-
-        while (node != null) {
-            number = number.multiply(BigInteger.TEN).add(BigInteger.valueOf(node.val));
-            node = node.next;
-        }
-
-        return number.toString().substring(1);
-    }
-
-    public BigInteger reverseNumber(String number) {
-        return new BigInteger(new StringBuilder(number).reverse().toString());
-    }
-
-    public ListNode buildNode(BigInteger number) {
         ListNode head = new ListNode();
-        ListNode node = head;
-        while (number.compareTo(BigInteger.ZERO) > 0) {
-            BigInteger remainder = number.mod(BigInteger.TEN);
-            number = number.divide(BigInteger.TEN);
-            node.val = remainder.intValue();
-            if (number.compareTo(BigInteger.ZERO) == 0) break;
-            node.next = new ListNode();
-            node = node.next;
+        ListNode dummy = head;
+
+        int carry = 0;
+        while (l1 != null || l2 != null) {
+            int x = l1 == null ? 0 : l1.val;
+            int y = l2 == null ? 0 : l2.val;
+
+            int sum = x + y + carry;
+            int r = sum % 10;
+            carry = sum / 10;
+
+            dummy.next = new ListNode();
+            dummy.next.val = r;
+            dummy = dummy.next;
+
+            if (l1 != null) l1 = l1.next;
+            if (l2 != null) l2 = l2.next;
         }
-        return head;
+
+        if (carry > 0) {
+            dummy.next = new ListNode(carry);
+        }
+
+        return head.next;
     }
 
     public static class ListNode {
         int val;
         ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
     }
 }
