6874f6c5979f83b48f2d95102b34555be9798c06
raychong <raychongtk@gmail.com>
2021-09-07 20:46:50 +0800

    missing number

--- src/array/MissingNumber.java
+++ src/array/MissingNumber.java
@@ -6,9 +6,10 @@ package array;
 public class MissingNumber {
     public static void main(String[] args) {
         System.out.println(missingNumber(new int[]{3, 0, 1}));
+        System.out.println(missingNumberV2(new int[]{3, 0, 1}));
     }
 
-    public static int missingNumber(int[] nums) {
+    private static int missingNumber(int[] nums) {
         int n = nums.length;
         int result = 0;
         for (int i = 0; i <= n; i++) {
@@ -20,5 +21,26 @@ public class MissingNumber {
         }
 
         return result;
+    }
+
+    private static int missingNumberV2(int[] nums) {
+        int start = 0;
+        int end = nums.length;
+
+        while (start < end) {
+            int current = nums[start];
+            if (current < end && nums[start] != nums[current]) {
+                nums[start] = nums[current];
+                nums[current] = current;
+            } else {
+                start++;
+            }
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != i) return i;
+        }
+
+        return end;
     }
 }
