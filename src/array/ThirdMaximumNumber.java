d9dbc7d7816fbf897568df74285694208596e414
raychong <raychongtk@gmail.com>
2021-09-29 00:16:59 +0800

    third maximum number

---
+++ src/array/ThirdMaximumNumber.java
@@ -0,0 +1,27 @@
+package array;
+
+/**
+ * @author raychong
+ */
+public class ThirdMaximumNumber {
+    public int thirdMax(int[] nums) {
+        long first = Long.MIN_VALUE;
+        long second = Long.MIN_VALUE;
+        long third = Long.MIN_VALUE;
+
+        for (int num : nums) {
+            if (num > first) {
+                third = second;
+                second = first;
+                first = num;
+            } else if (num < first && num > second) {
+                third = second;
+                second = num;
+            } else if (num < second && num > third) {
+                third = num;
+            }
+        }
+
+        return third > Long.MIN_VALUE ? (int) third : (int) first;
+    }
+}
