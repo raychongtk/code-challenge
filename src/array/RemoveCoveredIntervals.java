5759a71eb652c66ec6d63fcb8ddf1b6f7a935915
raychong <raychongtk@gmail.com>
2022-05-27 22:31:04 +0800

    update variable naming

--- src/array/RemoveCoveredIntervals.java
+++ src/array/RemoveCoveredIntervals.java
@@ -10,12 +10,12 @@ public class RemoveCoveredIntervals {
     public int removeCoveredIntervals(int[][] intervals) {
         Arrays.sort(intervals, Comparator.comparingInt((int[] x) -> x[0]).thenComparing(Comparator.comparingInt((int[] x) -> x[1]).reversed()));
         int count = 1;
-        int x = intervals[0][0];
-        int y = intervals[0][1];
+        int start = intervals[0][0];
+        int end = intervals[0][1];
         for (int i = 1; i < intervals.length; i++) {
-            if (intervals[i][0] > x && intervals[i][1] > y) {
-                x = intervals[i][0];
-                y = intervals[i][1];
+            if (intervals[i][0] > start && intervals[i][1] > end) {
+                start = intervals[i][0];
+                end = intervals[i][1];
                 count++;
             }
         }
