a97b953b48427e543937c23bea8d84a51020b9d3
raychong <raychongtk@gmail.com>
2022-06-08 21:42:53 +0800

    merge sorted array

--- src/array/MergeSortedArray.java
+++ src/array/MergeSortedArray.java
@@ -13,13 +13,21 @@ public class MergeSortedArray {
     // Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
     // Output: [1,2,2,3,5,6]
     public static void merge(int[] nums1, int m, int[] nums2, int n) {
-        if (n == 0) return;
+        int k = m + n - 1;
+        m--;
+        n--;
 
-        if (nums1.length - m >= 0) {
-            System.arraycopy(nums2, 0, nums1, m, nums1.length - m);
+        while (m >= 0 && n >= 0) {
+            if (nums1[m] < nums2[n]) {
+                nums1[k--] = nums2[n--];
+            } else {
+                nums1[k--] = nums1[m--];
+            }
         }
 
-        Arrays.sort(nums1);
+        while (n >= 0) {
+            nums1[k--] = nums2[n--];
+        }
         System.out.println(Arrays.toString(nums1));
     }
 }
