1ac649feddf5ffd9376315263a6b72ebecb40d5d
raychong <raychongtk@gmail.com>
2022-06-09 20:53:55 +0800

    two sum 2 with sorted array

---
+++ src/twopointer/TwoSumWithSortedArray.java
@@ -0,0 +1,25 @@
+package twopointer;
+
+/**
+ * @author raychong
+ */
+public class TwoSumWithSortedArray {
+    public int[] twoSum(int[] numbers, int target) {
+        int start = 0;
+        int end = numbers.length - 1;
+
+        while (start < end) {
+            int sum = numbers[start] + numbers[end];
+
+            if (sum == target) {
+                return new int[]{start + 1, end + 1};
+            } else if (sum > target) {
+                end--;
+            } else {
+                start++;
+            }
+        }
+
+        return new int[]{0, 0};
+    }
+}
