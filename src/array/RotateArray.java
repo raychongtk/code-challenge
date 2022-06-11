6196d53fa6092bfd3e279dc894844cbef6f19135
raychong <raychongtk@gmail.com>
2022-06-12 00:17:03 +0800

    rotate array

--- src/array/RotateArray.java
+++ src/array/RotateArray.java
@@ -9,11 +9,19 @@ public class RotateArray {
     }
 
     public static void rotate(int[] nums, int k) {
-        while (k > 0) {
-            int temp = nums[nums.length - 1];
-            System.arraycopy(nums, 0, nums, 1, nums.length - 1);
-            nums[0] = temp;
-            k--;
+        k %= nums.length;
+        reverse(nums, 0, nums.length - k - 1);
+        reverse(nums, nums.length - k, nums.length - 1);
+        reverse(nums, 0, nums.length - 1);
+    }
+
+    private static void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
         }
     }
 }
