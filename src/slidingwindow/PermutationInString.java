df41633f7beeeada88b8708d0b8b3d834c48d4e2
raychong <raychongtk@gmail.com>
2022-08-01 21:33:55 +0800

    permutation in string

---
+++ src/slidingwindow/PermutationInString.java
@@ -0,0 +1,37 @@
+package slidingwindow;
+
+public class PermutationInString {
+    public boolean checkInclusion(String s1, String s2) {
+        char[] table = new char[26];
+        int start = 0;
+        int windowSize = s1.length() - 1;
+        char[] frequencies = getFrequency(s1);
+
+        for (int end = 0; end < s2.length(); end++) {
+            table[s2.charAt(end) - 'a']++;
+            if (end - start == windowSize) {
+                boolean match = true;
+                for (int i = 0; i < 26; i++) {
+                    int frequency = frequencies[i];
+                    if (frequency == 0) continue;
+
+                    if (table[i] < frequency) {
+                        match = false;
+                        break;
+                    }
+                }
+                if (match) return true;
+                table[s2.charAt(start++) - 'a']--;
+            }
+        }
+        return false;
+    }
+
+    private char[] getFrequency(String str) {
+        char[] frequencies = new char[26];
+        for (char c : str.toCharArray()) {
+            frequencies[c - 'a']++;
+        }
+        return frequencies;
+    }
+}
