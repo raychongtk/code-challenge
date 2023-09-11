71540b35adb6885d91f4f04eacc2f44ca5a0506a
raychong <raychongtk@gmail.com>
2023-09-11 21:34:09 +0800

    group the people

--- src/hashtable/GroupThePeople.java
+++ src/hashtable/GroupThePeople.java
@@ -17,42 +17,21 @@ public class GroupThePeople {
         Input: groupSizes = [3,3,3,3,3,1,3]
         Output: [[5],[0,1,2],[3,4,6]]
      */
-    public static List<List<Integer>> groupThePeople(int[] groupSizes) {
-        Map<Integer, List<Integer>> map = new HashMap<>(500);
-        int left = 0;
-        int right = groupSizes.length - 1;
+    private static List<List<Integer>> groupThePeople(int[] groupSizes) {
+        List<List<Integer>> result = new ArrayList<>();
+        Map<Integer, List<Integer>> map = new HashMap<>();
 
-        for (int groupSize : groupSizes) {
-            map.computeIfAbsent(groupSize, k -> new ArrayList<>());
+        for (int i = 0; i < groupSizes.length; i++) {
+            List<Integer> list = map.getOrDefault(groupSizes[i], new ArrayList<>());
+            list.add(i);
+            map.put(groupSizes[i], list);
+
+            if (list.size() == groupSizes[i]) {
+                result.add(list);
+                map.remove(groupSizes[i]);
+            }
         }
 
-        while (left <= right) {
-            List<Integer> leftGroup = map.get(groupSizes[left]);
-            List<Integer> rightGroup = map.get(groupSizes[right]);
-
-            leftGroup.add(left);
-            map.put(groupSizes[left], leftGroup);
-
-            if (left != right) {
-                rightGroup.add(right);
-                map.put(groupSizes[right], rightGroup);
-            }
-
-            left++;
-            right--;
-        }
-
-        List<List<Integer>> groups = new ArrayList<>();
-        map.forEach((key, value) -> {
-            if (key == value.size()) {
-                groups.add(value);
-            } else {
-                for (int i = 0; i < value.size(); i += key) {
-                    groups.add(value.subList(i, i + key));
-                }
-            }
-        });
-
-        return groups;
+        return result;
     }
 }
